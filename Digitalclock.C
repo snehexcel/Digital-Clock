5627e5990c17e7b489fb61726755068ffe32ebf8
snehexcel <snehabaranwal5613@gmail.com>
2023-07-26 23:18:17 +0530

    Initial commit

---
+++ Digitalclock.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <windows.h>
+int main()
+
+{
+    int h,m,s;
+    int d=1000; // we add a delay of 1000 milliseconds and we will use it in the function sleep
+    printf("Set time :\n");
+    scanf("%d%d%d",&h,&m,&s);
+    if(h>12 || m>60 || s>60)
+    {
+        printf("Error ! \n");
+        exit(0);
+    }
+    while(1)// this is an infinite loup and anything inside will repeat itself to infinity
+    {
+        s++;
+        if(s>59)
+        {
+           m++;
+           s=0; 
+        }
+        if(m>59)
+        {
+            h++;
+            m=0;
+        }
+        if(h>12)
+        {
+            h=1;
+        }
+        printf("\n clock :");
+        printf("\n %02d:%02d:%02d",h,m,s); ths writes our time in this format 00:00:00
+        sleep(d);// the function sleep slow down the while loop make it more like a real clock
+        system("cls"); // this clears the screen
+        }
+    }
+}
